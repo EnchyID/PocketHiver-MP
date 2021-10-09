64f090d1061d507e57cb5ff8c0b6c9962de1a675
FrogasQ <91828204+FrogasQ@users.noreply.github.com>
2021-10-09 08:24:16 +0800

    Update ExampleUI.php

--- ExampleUI.php
+++ ExampleUI.php
@@ -32,9 +32,11 @@ class ExampleUI extends PluginBase implements Listener {
         foreach($this->getServer()->getOnlinePlayers() as $online){
             $buttons[] = ["text" => $online->getName()];
         }
-        $player->sendForm(new MenuForm("ONLINE LIST", "You've can see online in servers!", $buttons, function(Player $player, int $selected) use($buttons) : void {
-            $name = $buttons[$selected]["text"];
-            $player->sendMessage("You've selected player online of " . $name . "!");
-        }));
+        $player->sendForm(new MenuForm("ONLINE LIST", "You've can see online in servers!", $buttons, 
+            function(Player $player, int $selected) use($buttons) : void {
+                $name = $buttons[$selected]["text"];
+                $player->sendMessage("You've selected player online of " . $name . "!");
+            })
+        );
     }
 }
